e0aadfb84a00720611fc2004d7755e3bd50db111
Nelson Rivera <riveranelson99@gmail.com>
2021-10-10 16:54:05 -0500

    Finalization of assignment, possible final push

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 # weather_tracker
-Build an app that relies on thrid party APIs to help users track the weather according to specific locations.
+Build an app that relies on third party APIs to help users track the weather according to specific locations.
 
 # pseudo code
 
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
+* Create a weather tracker that provides information for multiple CITIES
+* When a user searches a city, weather information for current AND future days are provided
+* Weather information for the CURRENT day should provide the following information: City name, date, icon of expected weather, temperature, humidity, wind speed, and UV index
+* UV index of CURRENT day should indicate if it is favorable, moderate, or severe
+* Weather information for FUTURE days should provide the following information: date, icon of expected weather, temperature, wind speed, and humidity
+* Cities searched are to be stored in an interactable history in which the user can revisit any history they looked up
+* The weather tracker is to utilize BOTH the openweather API and local storage
 
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
+# screenshot
 
+![Generator Example](assets/images/port-6-screenshot.png)
 
-    * Uses the OpenWeather API to retrieve weather data.
+# link
 
-    * Uses `localStorage` to store persistent data.
+https://riveranelson99.github.io/weather_tracker/
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,24 @@
-// // Establish a function that FETCHES data from a weather api
-// // This function will be a CLICK function that activates when the user hits search
-// // Parse that info into data that will then be distributed into specific information
-// // That information will detail the city, date, temperature, wind, humidity, and UV index for the CURRENT date
-// // Below that information will be the same data across 5 additional days
-// // The 5 day forecast will be smaller in size (Small boxes) and contain image icons correlated to the weather expected fro that day (icons)
-// // The user will be able to search by different city's
-// // The city's will be put into a history bank to be referenced back to even after further searches
-// var $weatherServer = 'url';
-// var $key = "798aa8a41a70442411dba5a35b70bb2d";
+/*
+Establish a function that FETCHES data from a weather api
+This function will be a CLICK function that activates when the user hits search
+Parse that info into data that will then be distributed into specific information
+That information will detail the city, date, temperature, wind, humidity, and UV index for the CURRENT date
+Below that information will be the same data across 5 additional days
+The 5 day forecast will be smaller in size (Small boxes) and contain image icons correlated to the weather expected fro that day (icons)
+The user will be able to search by different city's
+The city's will be put into a history bank to be referenced back to even after further searches
+*/
 
-// var $api = `https://api.openweathermap.org/data/2.5/weather?q=Texas&appid=${$key}`;
-
-
-
-// function getWeather () {
-//     fetch ($api)
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-
-//         var $cityName = data.name;
-//         // var $temp = data.main.temp;
-//         var $temp = parseFloat((data.main.temp-273.15)*1.8)+32;
-//         $temp = $temp.toFixed(2);
-//         var $wind = data.wind.speed;
-//         var $hum = data.main.humidity;
-
-//         $("#city-name").append(`${$cityName} </br>`);
-
-//         var $testCard = `
-//         temp: ${$temp} </br>
-//         wind: ${$wind} </br>
-//         hum: ${$hum} </br>`
-
-//         $(".list-group").append($testCard);
-
-//         var $lat = data.coord.lat;
-//         var $lon = data.coord.lon;
-//         var $apiToo = `https://api.openweathermap.org/data/2.5/onecall?lat=${$lat}&lon=${$lon}&appid=${$key}`
-//         fetch($apiToo)
-//         .then( function (response) {
-//             return response.json();
-//         })
-//         .then(function (dataToo) {
-//             console.log(dataToo);
-
-//             var $uv = dataToo.current.uvi;
-
-//             $(".list-group").append(`uv: ${$uv}`);
-
-//         })
-//     })
-// }
-
-// getWeather();
-
-// // button.on("click", getWeather);
-
-// {/* <h5>${$cityName}</h5> </br> */}
-
+// Establish my key as a variable so as to not constantly type it out for every instance it is needed
+// Establish the search/submit button as a variable simply to call on it for the event listener
 var $key = `798aa8a41a70442411dba5a35b70bb2d`;
 var $submit = document.getElementById("submit");
 
-function getWeather(search) { // Incorporate this into the search function later
+// The first function to be established is the one that gets the current days weather
+// Utilize a fetch call to get the information from the openweather api
+// With that info, fill out the first container with todays weather info
+// The info to utilize will be the temp, wind, humidity, and uv index
+function getWeather(search) {
     var $api = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${$key}&units=imperial`;
 
     fetch($api)
@@ -73,9 +27,10 @@ function getWeather(search) { // Incorporate this into the search function later
     })
     .then(function(data) {
         console.log(data);
-        $("#city-name").append(data.name);
+        var $cityName =document.getElementById("city-name")
+        $cityName.textContent = "Showing Weather For: ";
 
-        var $today = $("#today");
+        var $today = document.getElementById("today");
         $today.textContent = "";
 
         var $cardHead = document.createElement("h3");
@@ -103,17 +58,29 @@ function getWeather(search) { // Incorporate this into the search function later
         var $uvIndex = document.createElement('p');
         $uvIndex.className = "card-text";
 
+        // A separate fetch call must be made for uv index as the initial call didn't contain such info
+        // Provide indicator of whether the uv index is favorable for the given day
         fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=${$key}&lat=${data.coord.lat}&lon=${data.coord.lon}`)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
             $uvIndex.textContent = `UV Index: ${data.value}`;
+
+            if (data.value < 3){
+                $uvIndex.className = "badge bg-secondary bg-success fs-5";
+            } else if (data.value < 7) {
+                $uvIndex.className = "badge bg-secondary bg-warning fs-5";
+            } else {
+                $uvIndex.className = "badge bg-secondary bg-danger fs-5";
+            }
         });
 
+        // Final step is to apply the weather forecast icon associated with the days weather to the card info
         var $weatherImg = document.createElement("img");
         $weatherImg.setAttribute("src", `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
 
+        $("#city-name").append(data.name);
         $cardHead.append($weatherImg);
         $card.append($cardHead);
         $card.append($temp);
@@ -125,12 +92,15 @@ function getWeather(search) { // Incorporate this into the search function later
     })
 }
 
-function fiveDay(search) { // Incorporate this into the search function later
+// The second function to be established is that of the 5 day forecast
+// Much similar to the first function, a fetch call will be made to gather the information from api
+// Structure of info is to be set up similarly to the first card, however set across 5 additional and smaller cards
+function fiveDay(search) {
     var $fiveHead = $("#five-head");
 
     $fiveHead.removeClass();
 
-    var $forecast = $("#forecast");
+    var $forecast = document.getElementById("forecast");
 
     $forecast.innerHTML = "";
 
@@ -144,7 +114,12 @@ function fiveDay(search) { // Incorporate this into the search function later
         console.log(data);
         var $forecastTwo = document.getElementById("forecast");
 
-        for (var i=4; i < data.list.length; i+=8) {
+        // Establish for loop with specific parameters to get 5 days of weather
+        // Failure to set up specific parameters resulted in weather forecast for every 3 hours
+        // Start at index 6 and get info from every additional 8 indexes
+        // This provides the weather information for every other day at exactly noon
+        // Noon is chosen as that is GENERALLY considered to be the highest temp point for the day
+        for (var i=6; i < data.list.length; i+=8) {
             console.log(data);
             var $cardHead = document.createElement('h5');
             $cardHead.className = "card-title text-left";
@@ -154,7 +129,7 @@ function fiveDay(search) { // Incorporate this into the search function later
             $cardCon.className = "card col-12 col-md-2 m-2 bg-success text-white";
 
             var $card = document.createElement('div');
-            $card.className = "card-body";
+            $card.className = "card-body p-2";
 
             var $cardTwo = document.createElement('div');
 
@@ -185,18 +160,62 @@ function fiveDay(search) { // Incorporate this into the search function later
     })
 }
 
-function searchCity(event) {
-    event.preventDefault();
-
+// The third function to be established is that of the search the user inputs
+// Whatever city is entered, that value is captured and sent into the previous two functions to output the weather info for whichever specific location
+// Additional functions to be added are to add the search to local storage and to set the city in a city history so as to be recalled upon if so desired
+function searchCity() {
     var $lookup = document.getElementById("city").value;
     if (document.getElementById("city").value !== ""){
         getWeather($lookup);
         fiveDay($lookup);
+        setSearch($lookup);
+        storeSearch();
 
         document.getElementById("city").value = "";
     }
 }
 
-// getWeather();
-// fiveDay();
+// Establish an additional variable to simply check if there are any cities stored in local storage
+var searchMemory;
+    if (JSON.parse(localStorage.getItem("history")) != null){        
+        searchMemory = JSON.parse(localStorage.getItem("history"));
+    } else {
+        searchMemory = [];
+    }
+
+// The fourth function is to set the search value the user input into local memory
+function setSearch(search) {
+    if (!searchMemory.includes(search)) {
+        searchMemory.push(search);
+        localStorage.setItem("history", JSON.stringify(searchMemory));
+    }
+}
+
+// The fifth function is to apply the city that is searched into the searched cities list
+function storeSearch() {
+    while (document.getElementById("history").firstChild) {
+        document.getElementById("history").removeChild(document.getElementById("history").firstChild);
+    }
+    citiesList();
+}
+
+// The sixth function is to populate a list in accordance to the users cities searched
+// The cities will be given event listeners so as to be clicked again should the user wish to revisit the information related to a city already searched for
+function citiesList() {
+    searchMemory.forEach(function(search) {
+        var $city = document.createElement("li");
+        $city.className = "list-group-item";
+        $city.textContent = search;
+
+        $city.addEventListener("click", function(event) {
+            getWeather(event.target.textContent);
+            fiveDay(event.target.textContent);
+        });
+        document.getElementById("history").appendChild($city);
+    });
+}
+
+// Invoke citiesList function to simply have the list of searched cities populate on page load should there be any cities stored in local data
+// Have an event listener for when the user clicks on search/submit
+citiesList();
 $submit.addEventListener("click", searchCity);
